447388471130c3fade2657101b8f6c630fd74aec
Sandiso Madayi <sandisomadayi239@gmail.com>
2019-07-17 15:55:36 +0200

    user must add both quantity and cost

--- AppleStockCount.java
+++ AppleStockCount.java
@@ -9,30 +9,28 @@ public class AppleStockCount {
 			int quantity = Integer.parseInt(args[0]);
 			double cost = Double.parseDouble(args[1]);
 			double total = quantity * cost;
-			int messages = quantity / 12;
+			int dozens = quantity / 12;
 			if (quantity == 0 || cost == 0) {
 				System.out.println("Price and cost need to be greater than zero.");
 				return;
 			} else {
 				message.append("You have bought ");
 				if (quantity % 12 == 0) {
-					message.append(messages);
+					message.append(dozens);
 					message.append(" dozen");
-					if (messages > 1) {
+					if (dozens > 1) {
 						message.append("s");
 					}
-					message.append(" of apples for a total of R");
-					message.append(total);
+					message.append(" of apples");
 				} else {
-					message.append("You bought ");
 					message.append(quantity);
 					message.append(" apple");
 					if (quantity > 1) {
 						message.append("s");
 					}
-					message.append(" for a total of R");
-					message.append(total);
 				}
+				message.append(" for a total of R");
+				message.append(total);
 			}
 			System.out.println(message.toString());
 		}
