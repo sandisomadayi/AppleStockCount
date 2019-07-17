8478dc9e9dbe84e904b07ccedef95d1293d24a20
Sandiso Madayi <sandisomadayi239@gmail.com>
2019-07-17 15:49:24 +0200

    user must add both quantity and cost

--- AppleStockCount.java
+++ AppleStockCount.java
@@ -2,42 +2,39 @@ public class AppleStockCount {
 	public static void main(String[] args) {
 
 		StringBuilder message = new StringBuilder();
-		int quantity = Integer.parseInt(args[0]);
-		double cost = Double.parseDouble(args[1]);
-		double total = quantity * cost;
-		int messages = quantity/12;
-
-		if (quantity <= 0) {
-			System.out.println("Out of stock.");
+		if (args.length < 2) {
+			System.out.println("Please enter both quantity and cost.");
 			return;
-		}
-		else if (cost <= 0) {
-			System.out.println("Price cannot be less than zero");
-			return;
-		}
-		else {
-			if (quantity%12 == 0) {
+		} else {
+			int quantity = Integer.parseInt(args[0]);
+			double cost = Double.parseDouble(args[1]);
+			double total = quantity * cost;
+			int messages = quantity / 12;
+			if (quantity == 0 || cost == 0) {
+				System.out.println("Price and cost need to be greater than zero.");
+				return;
+			} else {
 				message.append("You have bought ");
-				message.append(messages);
-				message.append(" dozen");
-				if (messages > 1) {
-					message.append("s");
+				if (quantity % 12 == 0) {
+					message.append(messages);
+					message.append(" dozen");
+					if (messages > 1) {
+						message.append("s");
+					}
+					message.append(" of apples for a total of R");
+					message.append(total);
+				} else {
+					message.append("You bought ");
+					message.append(quantity);
+					message.append(" apple");
+					if (quantity > 1) {
+						message.append("s");
+					}
+					message.append(" for a total of R");
+					message.append(total);
 				}
-				message.append(" of apples for a total of R");
-				message.append(total);
-
 			}
-			else {
-				message.append("You bought ");
-				message.append(quantity);
-				message.append(" apple");
-				if (quantity > 1) {
-					message.append("s");
-				}
-				message.append(" for a total of R");
-				message.append(total);
-			}
+			System.out.println(message.toString());
 		}
-		System.out.println(message.toString());
 	}
 }
